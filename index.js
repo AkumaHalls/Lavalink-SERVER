3d1dccc6f03e3242c6b2cca0a64f9253e4c5dd9f
AkumaHalls <playereagle16@gmail.com>
2024-09-08 18:17:58 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,16 @@
+const { default: { stream } } = require("got");
+const { createWriteStream } = require("fs");
+const { execSync } = require("child_process");
+const path = "./Lavalink.jar";
+
+// Always pull latest v3
+const lljar = "https://cdn.darrennathanael.com/jars/Lavalink.jar";
+
+const start = () => {
+     const download = stream(lljar).pipe(createWriteStream('Lavalink.jar'));
+     download.on("finish", () => {
+         execSync("java -jar Lavalink.jar", { stdio: "inherit" });
+     });
+ };
+
+start();
